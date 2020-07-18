b73f57d50af02fd156174f4b0bf72313ccff82d7
acpn <ant.pnasc@gmail.com>
2020-07-18 19:20:41 -0300

    Initial commit

---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,108 @@
+import React, { useRef, useCallback } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+
+import ValidationErrors from '../../utils/ValidationErrors';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, AnimationContainer, Content, Background } from './styles';
+
+interface ISignInFormData {
+  email: string;
+  password: string;
+}
+
+const Signin: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
+
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: ISignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = ValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description:
+            'Ocorreu um erro ao fazer login, verifique as credenciais.',
+        });
+      }
+    },
+    [signIn, addToast, history],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
+
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
+
+            <Button type="submit">Entrar</Button>
+
+            <a href="forgot">Esqueci minha senha</a>
+          </Form>
+
+          <Link to="/signup">
+            <FiLogIn />
+            Criar conta
+          </Link>
+        </AnimationContainer>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
+
+export default Signin;
